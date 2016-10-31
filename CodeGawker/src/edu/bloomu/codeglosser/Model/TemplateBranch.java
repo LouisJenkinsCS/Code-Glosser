845180ac9e2ad617efbee451da0676982c436c1b
Louis Jenkins <theif517@gmail.com>
2016-10-31 12:52:10 -0400

    Began implementation of JSON parsing to allow templates.

---
+++ CodeGawker/src/edu/bloomu/codeglosser/Model/TemplateBranch.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.bloomu.codeglosser.Model;
+
+import java.util.HashMap;
+import java.util.function.Function;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+/**
+ *
+ * @author Louis
+ */
+public class TemplateBranch implements TreeViewBranch {
+    
+    public static final String KEY_BODY = "body";
+    public static final String KEY_CATEGORY = "category";
+    
+    private static final Logger LOG = Logger.getLogger(TemplateBranch.class.getName());
+    
+    private final HashMap<String, TreeViewNode> childrenMap = new HashMap<>();
+    private String title;
+    
+    public TemplateBranch(JSONObject data) {
+        LOG.info("Parsing Branch: " + data.toJSONString());
+        this.title = (String) data.get(KEY_TITLE);
+        
+        JSONArray arr = (JSONArray) data.get(KEY_BODY);
+        TreeViewNode[] children = new TreeViewNode[arr.size()];
+        for (int i = 0; i < arr.size(); i++) {
+            JSONObject obj = (JSONObject) arr.get(i);
+            // Is another branch...
+            if (obj.containsKey(KEY_CATEGORY)) {
+                children[i] = new TemplateBranch(obj);
+            } else {
+                children[i] = new TemplateLeaf((JSONObject) arr.get(i));
+            }
+        }
+        
+        childrenMap.putAll(
+                Stream.of(children)
+                .collect(Collectors
+                        .toMap(TreeViewNode::toString, Function.identity())
+                )
+        );
+    }
+
+    @Override
+    public TreeViewNode[] getChildren() {
+        return childrenMap.values().toArray(new TreeViewNode[childrenMap.size()]);
+    }
+
+    @Override
+    public String toString() {
+        return "TemplateBranch{" + "childrenMap=" + childrenMap + '}';
+    }
+    
+    
+}
---
+++ CodeGawker/src/edu/bloomu/codeglosser/Model/TemplateLeaf.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.bloomu.codeglosser.Model;
+
+import java.awt.Color;
+import java.util.logging.Logger;
+import org.json.simple.JSONObject;
+
+/**
+ *
+ * @author Louis
+ */
+public class TemplateLeaf implements TreeViewLeaf {
+    
+    
+    private static final Logger LOG = Logger.getLogger(TemplateLeaf.class.getName());
+    
+    private final String prefix;
+    private final String message;
+    private final String title;
+    private final Color color;
+
+    public TemplateLeaf(JSONObject data) {
+        LOG.info("Parsing Leaf:" + data.toJSONString());
+        this.prefix = (String) data.get(KEY_PREFIX);
+        this.title = (String) data.get(KEY_TITLE);
+        this.message = (String) data.get(KEY_MESSAGE);
+        this.color = Color.decode((String) data.get(KEY_COLOR));
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+}
